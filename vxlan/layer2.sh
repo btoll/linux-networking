4dfdb2f87838de686d74bdec66abebadc78a059d
Benjamin Toll <ben@benjamintoll.com>
2026-08-23 02:22:39 -0400

    Improvements to layer2.sh
    - Allow for dynamic VTEP remote IP address assignment
    - Added status function to print network topology

--- vxlan/layer2.sh
+++ vxlan/layer2.sh
@@ -21,18 +21,18 @@
 #               root namespace bridge
 
 # root ns
-# - root-br0 bridge virtual interface (no IP)
-# - root-veth0 plugged into root-br0 (no IP)
-# - root-veth1 plugged into root-br0 (no IP)
+# - host-br0 bridge virtual interface (no IP)
+# - host-veth0 plugged into host-br0 (no IP)
+# - host-veth1 plugged into host-br0 (no IP)
 #
 # host0 ns
-# - root-ceth0 connected to root-veth0 in root ns (underlay IP 172.16.0.2/16)
+# - host-ceth0 connected to host-veth0 in root ns (underlay IP 172.16.0.1/16)
 # - br0 virtual bridge interface (no IP)
 # - veth0 plugged into br0 (no IP)
 # - vxlan100 tunneled to vxlan100 in host1 ns (no IP)
 #
 # host1 ns
-# - root-ceth1 connected to root-veth1 in root ns (underlay IP 172.16.0.3/16)
+# - host-ceth1 connected to host-veth1 in root ns (underlay IP 172.16.0.2/16)
 # - br0 virtual bridge interface (no IP)
 # - veth1 plugged into br0 (no IP)
 # - vxlan100 tunneled to vxlan100 in host0 ns (no IP)
@@ -43,6 +43,8 @@ LANG=C
 umask 0022
 
 ERROR="\e[31m[ERROR]\e[0m"
+INFO="\e[34m[INFO]\e[0m"
+#YELLOW="\e[43m[INFO]\e[0m"
 
 if [ $EUID -ne 0 ]; then
     printf "%b This script must be run as root!\n" "$ERROR" 1>&2
@@ -52,17 +54,12 @@ fi
 trap cleanup ERR
 
 cleanup() {
-    for ((i=0; i < HOSTS; i++))
+    local ns
+    for ns in $(ip netns list | awk '/host[0-9]+(-container[0-9]+)?/ {print $1}')
     do
-        ip netns delete "host$i"
-        for ((n=0; n < CONTAINERS; n++))
-        do
-            # Removing the namespace will also remove the interfaces within it,
-            # which subsequently also removes the other end of the pair in the
-            # root network namespace.
-            ip netns delete "host${i}-container$n"
-        done
+        ip netns delete "$ns"
     done
+    ip link delete "host$BRIDGE" type bridge 2> /dev/null
 }
 
 create_host() {
@@ -107,6 +104,8 @@ create_host() {
         # (the "cable" plugged into the bridge does NOT get an IP address).
         ip -netns "$containerns" address add "$first_three_octets.$((last_octet + CONTAINER_COUNTER))/$CONTAINER_NETMASK" dev "ceth$n"
         ip -netns "$containerns" link set "ceth$n" up
+        # Bring up loopback (optional).
+        ip -netns "$containerns" link set lo up
 
         CONTAINER_COUNTER=$((CONTAINER_COUNTER + 1))
     done
@@ -115,6 +114,7 @@ create_host() {
 create_root_bridge() {
     local first_three_octets="${HOST_BRIDGE_IP%.*}"
     local last_octet="${HOST_BRIDGE_IP##*.}"
+    local i
 
     ip link add name "host$BRIDGE" type bridge
 #    ip address add "$HOST_BRIDGE_IP/$HOST_NETMASK" dev "host$BRIDGE"
@@ -138,28 +138,103 @@ create_root_bridge() {
 setup_vxlan_vteps() {
     local vni=100
     local vtep="vxlan$vni"
+    local index
+    local local_ip
+    local peer_index
+    local peer_ip
 
-    ip -netns host0 link add "$vtep" type vxlan \
-        id "$vni" \
-        local 172.16.0.1 \
-        remote 172.16.0.2 \
-        dev host-ceth0 \
-        dstport 4789
+    for ((index = 0; index < HOSTS; index++)); do
+        local_ip="${HOST_BRIDGE_IP%.*}.$(( ${HOST_BRIDGE_IP##*.} + index ))"
 
-    ip -netns host0 link set dev "$vtep" master br0
-    ip -netns host0 link set "$vtep" up
-    ip -netns host0 link set "$vtep" mtu 1450
+        ip -netns "host$index" link add "$vtep" type vxlan \
+            id "$vni" \
+            local "$local_ip" \
+            dev "host-ceth$index" \
+            dstport 4789
 
-    ip -netns host1 link add "$vtep" type vxlan \
-        id "$vni" \
-        local 172.16.0.2 \
-        remote 172.16.0.1 \
-        dev host-ceth1 \
-        dstport 4789
+        ip -netns "host$index" link set dev "$vtep" master "$BRIDGE"
+        ip -netns "host$index" link set "$vtep" mtu 1450
+        ip -netns "host$index" link set "$vtep" up
 
-    ip -netns host1 link set dev "$vtep" master br0
-    ip -netns host1 link set "$vtep" up
-    ip -netns host1 link set "$vtep" mtu 1450
+        for ((peer_index = 0; peer_index < HOSTS; peer_index++)); do
+            [ "$peer_index" -eq "$index" ] && continue
+
+            peer_ip="${HOST_BRIDGE_IP%.*}.$(( ${HOST_BRIDGE_IP##*.} + peer_index ))"
+
+            ip netns exec "host$index" bridge fdb append \
+                00:00:00:00:00:00 \
+                dev "$vtep" \
+                dst "$peer_ip" \
+                self permanent
+        done
+    done
+}
+
+status() {
+    local h=()
+    local c=()
+    local a
+    local ns
+    local v
+    local line
+
+    printf "%b net namespaces\n" "$INFO"
+    # Remove the network namespace IDS, i.e., `host0 (id: 0)`.
+    # This is safe b/c there cannot be a space in a network name.
+    for ns in $(ip netns list | awk '{print $1}')
+    do
+        printf "%b \t\t%s\n" "$INFO" "$ns"
+        if [[ "$ns" =~ - ]]
+        then
+            c+=("$ns")
+        else
+            h+=("$ns")
+        fi
+    done
+
+    printf "%b \n" "$INFO"
+
+    printf "%b root namespace\n" "$INFO"
+    while read -r line
+    do
+        printf "%b \t\t%s\n" "$INFO" "$line"
+    done < <(ip -br a)
+
+    printf "%b \n" "$INFO"
+
+    for a in h c
+    do
+        # Create a nameref (`current_array`) that points to the array name stored in `a`.
+        declare -n current_array="$a"
+        for v in "${current_array[@]}"
+        do
+            if [[ ! ( "$v" =~ - ) ]]
+            then
+                printf "%b %s - bridge fdb\n" "$INFO" "$v"
+                while read -r line
+                do
+                    printf "%b \t\t%s\n" "$INFO" "$line"
+                done < <(ip netns exec "$v" bridge fdb show br "$BRIDGE")
+
+                printf "%b \n" "$INFO"
+
+                printf "%b %s - vxlan fdb\n" "$INFO" "$v"
+                while read -r line
+                do
+                    printf "%b \t\t%s\n" "$INFO" "$line"
+                done < <(ip netns exec "$v" bridge fdb show dev vxlan100)
+
+                printf "%b \n" "$INFO"
+            fi
+
+            printf "%b %s\n" "$INFO" "$v"
+            while read -r line
+            do
+                printf "%b \t\t%s\n" "$INFO" "$line"
+            done < <(ip -n "$v" -br a)
+            printf "%b \n" "$INFO"
+        done
+    done
 }
 
 if ! command -v ipcalc > /dev/null
@@ -177,6 +252,7 @@ usage() {
     printf -- "--host-cidr       : The CIDR address for the hosts (defaults to 172.16.0.0/16.\n"
     printf -- "--hosts           : A host should be thought of as a node or VM (defaults to 2).\n"
     printf -- "                    Each host will get two containers (i.e., its own isolated net namespace).\n"
+    printf -- "--status          : Prints the network topology.\n"
     printf -- "-h, --help        : Show usage.\n"
     exit "$1"
 }
@@ -188,6 +264,7 @@ CONTAINERS=2
 HOST_CIDR=172.16.0.0/16
 DESTROY=
 HOSTS=2
+STATUS=
 
 while [ "$#" -gt 0 ]
 do
@@ -199,6 +276,7 @@ do
         --host-cidr) shift; HOST_CIDR=$1 ;;
         --hosts) shift; HOSTS=$1 ;;
         -h|--help) usage 0 ;;
+        --status) STATUS=1 ;;
         *) printf "Unknown flag %s\n" "$OPT"; usage 1 ;;
     esac
     shift
@@ -207,6 +285,9 @@ done
 if [ -n "$DESTROY" ]
 then
     cleanup
+elif [ -n "$STATUS" ]
+then
+    status
 else
     # 172.18.0.0/20 - 4096 addresses
     # 172.18.0.0/21 - 2048 addresses
